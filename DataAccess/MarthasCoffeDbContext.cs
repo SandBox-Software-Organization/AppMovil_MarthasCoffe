921e92bf5cccfa41f0f79792be7c3f2841849065
yulissa22 <ec1413220@gmail.com>
2023-10-16 22:53:54 -0600

    Categorías

--- DataAccess/MarthasCoffeDbContext.cs
+++ DataAccess/MarthasCoffeDbContext.cs
@@ -80,198 +80,113 @@ namespace MarthasCoffe.DataAccess
 
 
             modelBuilder.Entity<Categoria>().HasData(
-                new Categoria { IdCategoria = 1, Descripcion = "jjjj", Imagen = "cup.jpg" },
-                new Categoria { IdCategoria = 2, Descripcion = "nnnnn", Imagen = "cup.jpg" },
-                new Categoria { IdCategoria = 3, Descripcion = "vhbjb", Imagen = "cup.jpg" },
-                new Categoria { IdCategoria = 4, Descripcion = "vhvkj", Imagen = "cup.jpg" },
-                new Categoria { IdCategoria = 5, Descripcion = "gvbhvh", Imagen = "cup.jpg" },
-                new Categoria { IdCategoria = 6, Descripcion = "vggjvhbk", Imagen = "cup.jpg" },
-                new Categoria { IdCategoria = 7, Descripcion = "bbbbb", Imagen = "cup.jpg" },
-                new Categoria { IdCategoria = 8, Descripcion = "bjbubb", Imagen = "cup.jpg" }
+                new Categoria { IdCategoria = 1, Descripcion = "Bebidas", Imagen = "cup.jpg" },
+                new Categoria { IdCategoria = 2, Descripcion = "Platillos", Imagen = "cup.jpg" },
+                new Categoria { IdCategoria = 3, Descripcion = "Bocadillos", Imagen = "cup.jpg" },
+            new Categoria { IdCategoria = 3, Descripcion = "Postres", Imagen = "cup.jpg" }
                 );
 
-            modelBuilder.Entity<Producto>().HasData(
-                new Producto
-                {
-                    IdProducto = 1,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 1,
-                    Precio = 2000,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 2,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 1,
-                    Precio = 2500,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 3,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 1,
-                    Precio = 3000,
-                    Imagen = "prod3.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 4,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 1,
-                    Precio = 3200,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 5,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 1,
-                    Precio = 1560,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 6,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 2,
-                    Precio = 500,
-                    Imagen = "prod6.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 7,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 2,
-                    Precio = 400,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 8,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 3,
-                    Precio = 500,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 9,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 3,
-                    Precio = 200,
-                    Imagen = "prod9.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 10,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 3,
-                    Precio = 450,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 11,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
-                    IdCategoria = 3,
-                    Precio = 2120,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 12,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "NINGUNO",
-                    IdCategoria = 4,
-                    Precio = 220,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 13,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "NINGUNO",
-                    IdCategoria = 4,
-                    Precio = 890,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 14,
-                    Nombre = "sdfsfcds",
-                    Descripcion = "TALLA: 4\r\nTALLA:4.5",
-                    IdCategoria = 5,
-                    Precio = 260,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 15,
-                    Nombre = "jsjsjnjj",
-                    Descripcion = "TALLA: 4\r\nTALLA:4.5",
-                    IdCategoria = 5,
-                    Precio = 230,
-                    Imagen = "cup.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 16,
-                    Nombre = "jsjsjnjj",
-                    Descripcion = "NINGUNO",
-                    IdCategoria = 6,
-                    Precio = 300,
-                    Imagen = "prod16.jpg"
-                },
-                new Producto
-                {
-                    IdProducto = 17,
-                    Nombre = "mkmkmkmk",
-                    Descripcion = "NINGUNO",
-                    IdCategoria = 6,
-                    Precio = 87,
-                    Imagen = "cup.jpg"
-                },
-                 new Producto
-                 {
-                     IdProducto = 18,
-                     Nombre = "njjnj",
-                     Descripcion = "COLOR: NARANJA",
-                     IdCategoria = 7,
-                     Precio = 90,
-                     Imagen = "cup.jpg"
-                 },
-                  new Producto
-                  {
-                      IdProducto = 19,
-                      Nombre = "njjj",
-                      Descripcion = "NINGUNO",
-                      IdCategoria = 7,
-                      Precio = 130,
-                      Imagen = "cup.jpg"
-                  },
-                  new Producto
-                  {
-                      IdProducto = 20,
-                      Nombre = "njjnjnjnjnj",
-                      Descripcion = "NINGUNO",
-                      IdCategoria = 7,
-                      Precio = 110,
-                      Imagen = "cup.jpg"
-                  }
-                );
+            //modelBuilder.Entity<Producto>().HasData(
+            //new Producto
+            //{
+            //  IdProducto = 1,
+            //Nombre = "sdfsfcds",
+            //Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
+            //IdCategoria = 1,
+            //Precio = 2000,
+            //Imagen = "cup.jpg"
+            //},
+            //new Producto
+            //{
+            //  IdProducto = 2,
+            //Nombre = "sdfsfcds",
+            //Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
+            //IdCategoria = 1,
+            //Precio = 2500,
+            //Imagen = "cup.jpg"
+            //},
+            //new Producto
+            //{
+            //  IdProducto = 3,
+            //Nombre = "sdfsfcds",
+            //Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
+            //IdCategoria = 1,
+            //Precio = 3000,
+            //Imagen = "prod3.jpg"
+            //},
+            //new Producto
+            //{
+            //IdProducto = 4,
+            //Nombre = "sdfsfcds",
+            //Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
+            //IdCategoria = 1,
+            //Precio = 3200,
+            //Imagen = "cup.jpg"
+            //},
+            //new Producto
+            //{
+            //  IdProducto = 5,
+            //Nombre = "sdfsfcds",
+            //Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
+            //IdCategoria = 1,
+            //Precio = 1560,
+            //Imagen = "cup.jpg"
+            //},
+            //new Producto
+            //{
+            //  IdProducto = 6,
+            //Nombre = "sdfsfcds",
+            //Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
+            //IdCategoria = 2,
+            //Precio = 500,
+            //Imagen = "prod6.jpg"
+            //},
+            //new Producto
+            //{
+            //  IdProducto = 7,
+            //Nombre = "sdfsfcds",
+            //Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
+            //IdCategoria = 2,
+            //Precio = 400,
+            // Imagen = "cup.jpg"
+            //},
+            // new Producto
+            //{
+            //  IdProducto = 8,
+            // Nombre = "sdfsfcds",
+            //Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
+            //IdCategoria = 3,
+            //Precio = 500,
+            //Imagen = "cup.jpg"
+            //},
+            //new Producto
+            //{
+            //  IdProducto = 9,
+            //Nombre = "sdfsfcds",
+            //Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
+            //IdCategoria = 3,
+            //Precio = 200,
+            //Imagen = "prod9.jpg"
+            //},
+            //new Producto
+            //{
+            //  IdProducto = 10,
+            //Nombre = "sdfsfcds",
+            //Descripcion = "jsjsjs\r\nxxxxxD\r\njsjjsjsjsj\r\nssnsjssj\r\njsjsjsjsjjsjsjs",
+            //IdCategoria = 3,
+            //Precio = 450,
+            //Imagen = "cup.jpg"
+            //},
+            //new Producto
+            //{
+              //  IdProducto = 20,
+                //Nombre = "njjnjnjnjnj",
+                //Descripcion = "NINGUNO",
+                //IdCategoria = 7,
+                //Precio = 110,
+                //Imagen = "cup.jpg"
+            //}
+                //);
         }
     }
 }
